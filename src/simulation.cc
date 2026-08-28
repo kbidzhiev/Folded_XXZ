255b8b8518f87cf07325c631c965fb483d2d5177
Kemal Bidzhiev <kem.bidzhiev@gmail.com>
2026-08-28 12:52:39 +0200

    initial state

--- src/simulation.cc
+++ src/simulation.cc
@@ -2,53 +2,13 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <folded_xxz/initial_state.h>
 #include <folded_xxz/observables.h>
 #include <folded_xxz/parameters.h>
 #include <folded_xxz/profile.h>
 #include <folded_xxz/simulation_schedule.h>
 #include <folded_xxz/three_site_hamiltonian.h>
 #include <folded_xxz/trotter_evolution.h>
-
-itensor::MPS create_initial_state(const itensor::SiteSet &sites) {
-  const int site_count = itensor::length(sites);
-  itensor::MPS psi(sites);
-  for (int first_site = 1; first_site < site_count; first_site += 2) {
-    const auto left_site = sites(first_site);
-    const auto right_site = sites(first_site + 1);
-    itensor::ITensor wavefunction;
-
-    if (first_site == 1) {
-      const auto right_link =
-          itensor::commonIndex(psi(first_site + 1), psi(first_site + 2), "Link");
-      wavefunction = itensor::ITensor(left_site, right_site, right_link);
-      psi.ref(first_site) = itensor::ITensor(left_site);
-      psi.ref(first_site + 1) = itensor::ITensor(right_site, right_link);
-      wavefunction.set(left_site(1), right_site(2), right_link(1), itensor::ISqrt2);
-      wavefunction.set(left_site(2), right_site(1), right_link(1), -itensor::ISqrt2);
-    } else if (first_site == site_count - 1) {
-      const auto left_link = itensor::commonIndex(psi(first_site - 1), psi(first_site), "Link");
-      wavefunction = itensor::ITensor(left_link, left_site, right_site);
-      psi.ref(first_site) = itensor::ITensor(left_site, left_link);
-      psi.ref(first_site + 1) = itensor::ITensor(right_site);
-      wavefunction.set(left_link(1), left_site(1), right_site(2), itensor::ISqrt2);
-      wavefunction.set(left_link(1), left_site(2), right_site(1), -itensor::ISqrt2);
-    } else {
-      const auto left_link = itensor::commonIndex(psi(first_site - 1), psi(first_site), "Link");
-      const auto right_link =
-          itensor::commonIndex(psi(first_site + 1), psi(first_site + 2), "Link");
-      wavefunction = itensor::ITensor(left_link, left_site, right_site, right_link);
-      psi.ref(first_site) = itensor::ITensor(left_site, left_link);
-      psi.ref(first_site + 1) = itensor::ITensor(right_site, right_link);
-      wavefunction.set(left_link(1), left_site(1), right_site(2), right_link(1), itensor::ISqrt2);
-      wavefunction.set(left_link(1), left_site(2), right_site(1), right_link(1), -itensor::ISqrt2);
-    }
-
-    itensor::ITensor singular_values;
-    itensor::svd(wavefunction, psi.ref(first_site), singular_values, psi.ref(first_site + 1));
-    psi.ref(first_site) *= singular_values;
-  }
-  return psi;
-}
 
 void evolve_step(itensor::MPS &psi, int step, const SimulationSchedule &schedule, int center,
                  const itensor::Args &real_time_args, const itensor::Args &thermal_args,
